b4e7a49543a08474aeedcfa0e5d640ebb71b27f0
qmatias <none>
2021-09-04 02:43:26 -0500

    run formatter

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from http import client
 import os
 import statistics
 from datetime import datetime, timedelta
+from http import client
 from math import ceil
 from pprint import pprint
-from strategy import Strategy
 from typing import Optional
 
 import cbpro
 from dotenv import load_dotenv
 
 import api
+from strategy import Strategy
 
 
 def run_strategy(strategy_builder):
